64fbfcde88a7b2d83649e383fda4f2ddb9d20050
bashirnayal <bashirnayal.99@gmail.com>
2021-09-17 13:35:42 +0200

    init

---
+++ CMakeFiles/anp_server.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anp_server.dir/server-client/tcp_server.c.o"
+  "CMakeFiles/anp_server.dir/server-client/common.c.o"
+  "build/anp_server.pdb"
+  "build/anp_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/anp_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
