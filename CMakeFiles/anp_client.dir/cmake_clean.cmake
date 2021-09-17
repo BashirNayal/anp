c69228766ba01139169ca695116373f417674272
b <Bashir-Nayal>
2021-09-17 09:33:29 -0400

    test

---
+++ CMakeFiles/anp_client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/anp_client.dir/server-client/tcp_client.c.o"
+  "CMakeFiles/anp_client.dir/server-client/common.c.o"
+  "build/anp_client.pdb"
+  "build/anp_client"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/anp_client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
